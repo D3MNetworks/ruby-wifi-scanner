bb21dfb77bcfb9d9c722afe805747d797e8d5558
Uri Gorelik <uri.gore@gmail.com>
2019-05-27 17:06:17 -0400

    fix: Linux actually works now

--- lib/ruby_wifi_scanner/linux_scanner.rb
+++ lib/ruby_wifi_scanner/linux_scanner.rb
@@ -1,19 +1,27 @@
 module RubyWifiScanner
   class LinuxScanner < Scanner
-
     protected
 
     def parse
-      res = raw_scan.split "\n"
-      res[1..-1].map do |row|
-        m = row.strip.match(/(?<level>-?[\d.]{4,6})\s+(?<rssi>[a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2})\s*(?<ssid>\w+)?/i)
-        WifiInfo.new(m[:ssid], m[:rssi], m[:level]) if m
+      scan = raw_scan.scan(/
+        ^BSS\s([a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2}:[a-f0-9]{2})
+        .+?
+        signal:\s([\d.-]+?)\sdBm$
+        .+?
+        SSID:\s(\w+?)$
+      /mx)
+
+      scan.map do |rssi,level,ssid|
+        WifiInfo.new(ssid, rssi, level)
       end
     end
 
     def raw_scan
-      @raw_scan ||=
-        %x{sudo iw dev $DEVICE scan | awk -f wlan_scan.awk | sort}
+      return @raw_scan if @raw_scan
+
+      device = ENV["DEVICE"] || 'wlan0'
+
+      @raw_scan = %x{sudo iw dev #{device} scan}
     end
   end
 end
